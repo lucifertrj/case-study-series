889399a0ae7e40926879febfce3499d4af905ad4
lucifertrj <jaintarun.abd17@gmail.com>
2026-08-27 03:36:22 +0530

    include the ui workflow screens

--- rfp-generation/frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// Home redirects to /knowledge via next.config.ts.
-// This file exists so that /app has an index route in dev previews.
-export default function HomePage() {
-  return (
-    <div className="p-8 text-slate-600">
-      Redirecting to the company library...
-    </div>
-  );
-}
